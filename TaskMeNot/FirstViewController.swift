f2a9822f2d85337cbc979cf2fd14f581812fa813
Rohit Nisal <rohitnisal@gmail.com>
2016-12-26 20:49:59 -0800

    - File changes commit

--- TaskMeNot/FirstViewController.swift
+++ TaskMeNot/FirstViewController.swift
@@ -8,18 +8,59 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDataSource {
 
+   @IBOutlet weak var taskTableView: UITableView!
+   @IBOutlet weak var addBarButton: UIBarButtonItem!
+   @IBOutlet weak var taskSegementControl: UISegmentedControl!
+    
+    var numberOfTask = 10
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+      func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+      func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return numberOfTask
+    }
+    
+      func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell", for: indexPath)
+        
+        return cell
+    }
 
 
+    func updateTaskList() {
+        UIView.transition(with: self.taskTableView,
+                          duration:0.35,
+                          options:[.transitionFlipFromTop, .transitionCrossDissolve],
+                          animations:
+            { () -> Void in
+                self.taskTableView.reloadData()
+        },
+                          completion: nil);
+    }
+    
+    @IBAction func taskValueSelection(_ sender: Any) {
+        
+        if self.taskSegementControl.selectedSegmentIndex == 0 {
+            numberOfTask = 10
+        }else {
+            numberOfTask = 2
+        }
+        updateTaskList();
+    }
+    
 }
 
--- TaskMeNot/LoginViewController.swift
+++ TaskMeNot/LoginViewController.swift
@@ -6,4 +6,31 @@
 //  Copyright © 2016 Rohit Nisal. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var passwordTxtField: UITextField!
+    @IBOutlet weak var userNameTxtField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+//        let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.dark)
+//        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+//        blurEffectView.frame = view.bounds
+//        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+//        self.backgroundImageView.addSubview(blurEffectView)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func loginButtonClick(_ sender: Any) {
+        let delegate = UIApplication.shared.delegate as! AppDelegate;
+        delegate.showTaskBarController();
+    }
+}
